817f529cfa185f19433a3e26f8d8bf4e86e73fc9
TheKingPreto <lucasoliveirasilva2008@gmail.com>
2025-03-21 20:31:37 -0300

    encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction frac1 = new Fraction(); 
+        Fraction frac2 = new Fraction(6);     
+        Fraction frac3 = new Fraction(6, 7);   
+
+        Console.WriteLine(frac1.GetFractionString() + " = " + frac1.GetDecimalValue());
+        Console.WriteLine(frac2.GetFractionString() + " = " + frac2.GetDecimalValue());
+        Console.WriteLine(frac3.GetFractionString() + " = " + frac3.GetDecimalValue());
+
+        frac1.SetNumerator(3);
+        frac1.SetDenominator(4);
+
+        Console.WriteLine(frac1.GetFractionString() + " = " + frac1.GetDecimalValue());
     }
 }
